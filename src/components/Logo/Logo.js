1f8dce30ba09f367744eb792500462b6f6a36302
Angelxoxo757 <maria.cromety@gmail.com>
2021-01-04 17:07:36 -0500

    Added react logo picture

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Tilt from 'react-tilt'
+import Tilt from 'react-tilt';
+import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma3 mt0'>
-            <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner br2 shadow-2"> 👽 </div>
+            <Tilt className="Tilt  br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner"> 👽 </div>
             </Tilt>
         </div>
     );
